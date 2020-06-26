4f497c62de39592cf3fed8e816dcf4981a9288ca
Prabhat Bharadwaj <60924493+prabhat1209@users.noreply.github.com>
2020-06-26 09:22:58 +0530

    Mentioning Analysis

--- Tower_of_Hanoi.cpp
+++ Tower_of_Hanoi.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+// As recursion approach is followed to solve the problem so... 
+// Time Complexity - O(2^n)
+
 void TOH(int n,int A,int B,int C){
     if(n>0){
         TOH(n-1,A,C,B);
