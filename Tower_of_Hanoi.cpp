6eeed4a889e855ffba3469b229a28b326af61ead
prabhat1209 <prabhatbharadwaj33@gmail.com>
2020-08-06 17:12:44 +0530

    Improving code

--- Tower_of_Hanoi.cpp
+++ Tower_of_Hanoi.cpp
@@ -6,9 +6,9 @@ using namespace std;
 
 void TOH(int n,int A,int B,int C){
     if(n>0){
-        TOH(n-1,A,C,B);
+        TOH(n-1,A,C,B);                 //Recursion
         cout<<"("<<A<<","<<C<<")"<<endl;
-        TOH(n-1,B,A,C);
+        TOH(n-1,B,A,C);			//Recursion
     }
 }
 
